e9e290399ad46fa2e32855209cd69d45a90a1a4f
Emircan <emircandv787@gmail.com>
2024-08-22 23:47:26 +0300

    The  RedisContext class has been defined .

---
+++ RealtimeScoreBoard.WebApp/Contexts/RedisContext.cs
@@ -0,0 +1,25 @@
+﻿using StackExchange.Redis;
+
+namespace RealtimeScoreBoard.WebApp.Contexts
+{
+    public class RedisContext
+    {
+        protected ConnectionMultiplexer _multiplexer;
+        public ConnectionMultiplexer ConnectionMultiplexer => _multiplexer;
+
+        public RedisContext(string connectionString)
+            => _multiplexer = ConnectionMultiplexer.Connect(connectionString);
+
+        public RedisContext(ConfigurationOptions options)
+            => _multiplexer = ConnectionMultiplexer.Connect(options);
+
+        public RedisContext(ConnectionMultiplexer multiplexer)
+            => _multiplexer = multiplexer;
+
+        public IDatabase GetDatabase()
+            => _multiplexer.GetDatabase();
+
+
+
+    }
+}
