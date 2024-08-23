f5703ee9a2ba1d618cff9027963e1b46a3588745
Emircan <emircandv787@gmail.com>
2024-08-23 04:48:29 +0300

    The IRedisSortedSetRepository and RedisSortedSetRepository have been defined.

---
+++ RealtimeScoreBoard.WebApp/Repositories/IRedisSortedSetRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace RealtimeScoreBoard.WebApp.Repositories
+{
+    public interface IRedisSortedSetRepository
+    {
+        public Task IncreaseScore(string key, string member, double score);
+
+        public Task DecreaseScore(string key, string memeber, double score);
+
+        public Task UpdateScore(string key, string member, double score);
+
+        public Task<IEnumerable<ValueTuple<string, string>>> GetScoresWithMembers(string key);
+
+    }
+}
---
+++ RealtimeScoreBoard.WebApp/Repositories/RedisSortedSetRepository.cs
@@ -0,0 +1,49 @@
+﻿
+using RealtimeScoreBoard.WebApp.Contexts;
+using StackExchange.Redis;
+
+namespace RealtimeScoreBoard.WebApp.Repositories
+{
+    public class RedisSortedSetRepository : IRedisSortedSetRepository /*, IDisposable*/
+    {
+        private readonly RedisContext _context;
+        private readonly IDatabase _database;
+        public RedisSortedSetRepository(RedisContext context)
+        {
+            _context = context;
+            _database = context.GetDatabase();
+        }
+
+        public async Task DecreaseScore(string key, string member, double score)
+        {
+            await _database.SortedSetDecrementAsync(key, member, score);
+        }
+
+        
+
+        public async Task<IEnumerable<(string, string)>?> GetScoresWithMembers(string key)
+        {
+            var membersWithScores = await _database.SortedSetRangeByRankWithScoresAsync(key);
+
+            return membersWithScores.Select(sse => 
+            ValueTuple.Create<string, string>(sse.Element, sse.Score.ToString()));
+
+        }
+
+        public async Task IncreaseScore(string key, string member, double score)
+        {
+            await _database.SortedSetIncrementAsync(key, member, score);
+        }
+
+        public async Task UpdateScore(string key, string member, double score)
+        {
+            await _database.SortedSetAddAsync(key, member, score);
+            
+        }
+
+        //public void Dispose()
+        //{
+        //    _context.Dispose();
+        //}
+    }
+}
