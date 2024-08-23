7721b38aa2dc0e9653d5bc2615d430722d882112
Emircan <emircandv787@gmail.com>
2024-08-23 11:16:57 +0300

    The home controller and view have been defined.

---
+++ RealtimeScoreBoard.WebApp/Controllers/Home.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealtimeScoreBoard.WebApp.Controllers
+{
+    public class Home : Controller
+    {
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+
+    }
+}
---
+++ RealtimeScoreBoard.WebApp/wwwroot/signalr/signalr_index_view.js
@@ -0,0 +1,65 @@
+﻿$(document).ready(function () {
+
+    console.log("documan yuklendi");
+
+    const hubUrl = "/scoreboardhub";
+
+    var connection = new signalR
+        .HubConnectionBuilder()
+        .withUrl(hubUrl)
+        .withAutomaticReconnect()
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+
+
+    const receiveScoreboardScores = "ReceiveScoreboardScores"; 
+
+    const decreaseScoreboardScore = "DecreaseScoreboardScore"
+    const updateScoreboardScore = "UpdateScoreboardScore";
+    const increaseScoreboardScore = "IncreaseScoreboardScore";
+
+
+    function startConnection () {
+
+        connection
+            .start()
+            .then(() => {
+                console.log("Bağlantı başarıyla kuruldu");
+            })
+            .catch(err => {
+                console.log("Bağlantı başlatılamadı", err);
+            })      
+
+
+    }
+
+    try {
+
+        startConnection();
+
+    }
+    catch (e) {
+        setTimeout(() => startConnection(), 5000);
+    }
+
+
+    connection.on(receiveScoreboardScores, (result) => {
+        console.log(result);
+
+    });
+
+
+    $("#increase-button").click(function () {
+
+        const member = $("#member").val();
+        const score = $("#score").val();
+
+        
+        connection
+            .invoke(increaseScoreboardScore, member, score)
+            .catch(err => console.log(err));
+    });
+
+
+
+})
